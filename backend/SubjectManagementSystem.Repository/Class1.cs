0ca3ecd18c69313b59977d8858b6c7a150ce0074
IsacJr <isac.aniceto@hotmail.com>
2021-11-13 23:55:25 -0300

    backend initial architecture

---
+++ backend/SubjectManagementSystem.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SubjectManagementSystem.Repository
+{
+    public class Class1
+    {
+    }
+}
---
+++ backend/SubjectManagementSystem.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SubjectManagementSystem.Service
+{
+    public class Class1
+    {
+    }
+}
