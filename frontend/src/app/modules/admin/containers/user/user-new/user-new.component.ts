d79ab80e147a2f8d59a78673f154f1ef1a757f62
Isac <isac.aniceto@hotmail.com>
2021-11-29 23:43:59 -0300

    Add style and mock to user form

--- frontend/src/app/modules/admin/containers/user/user-new/user-new.component.ts
+++ frontend/src/app/modules/admin/containers/user/user-new/user-new.component.ts
@@ -11,6 +11,18 @@ export class UserNewComponent implements OnInit {
 
   readonly PAGE_TITLE_LABEL = "Novo Usuário";
   userForm: FormGroup;
+
+  stateMock = [
+    { value: 0, name: 'Acre' },
+    { value: 1, name: 'Amapá' },
+    { value: 2, name: 'Bahia' }
+  ]
+
+  typeMock = [
+    { value: 0, name: 'Parceiro' },
+    { value: 1, name: 'Professor' },
+    { value: 2, name: 'Estudante' }
+  ]
   
   constructor(private formBuilder: FormBuilder, private userFacade: UserFacade) {
     this.userForm = this.formBuilder.group({
@@ -27,9 +39,19 @@ export class UserNewComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.userForm);
     const payload = this.userForm.value;
+    console.log(payload);
     this.userFacade.post(payload).subscribe(response => console.log(response));
+  }
+
+  changeState(event: any) {
+    this.userForm.get('state')?.setValue(+event.target.value);
+    console.log(event.target.value);
+  }
+
+  changeType(event: any) {
+    this.userForm.get('type')?.setValue(+event.target.value);
+    console.log(event.target.value);
   }
 
 }
