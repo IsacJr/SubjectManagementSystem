a3fac8ef1b2863ae11b6d8c2c82af299e3776738
Isac <isac.aniceto@hotmail.com>
2021-12-20 21:38:54 -0300

    Add initial my profile

--- frontend/src/app/modules/profile/containers/profile/profile.component.ts
+++ frontend/src/app/modules/profile/containers/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { UserFacade } from 'src/app/modules/admin/containers/user/user.facade';
+import { StateFacade } from 'src/app/shared/services/state.facade';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  stateList = [] as any[];
+  userTypeList = [] as any[];
+
+  userForm: FormGroup;
+  
+  constructor(private formBuilder: FormBuilder, private userFacade: UserFacade, private stateFacade: StateFacade) {
+    this.userForm = this.formBuilder.group({
+      name: '',
+      type: null,
+      email: '',
+      birthday: '',
+      city: '',
+      state: null,
+      password: ''
+    });
+
+    stateFacade.getAll().subscribe(response => this.stateList = response);
+    userFacade.getAllUserTypes().subscribe(response => this.userTypeList = response);
+  }
 
   ngOnInit(): void {
+  }
+
+  changeState(event: any) {
+    this.userForm.get('state')?.setValue(+event.target.value);
+  }
+
+  changeType(event: any) {
+    this.userForm.get('type')?.setValue(+event.target.value);
   }
 
 }
--- frontend/src/app/modules/profile/profile.module.ts
+++ frontend/src/app/modules/profile/profile.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProfileRoutes } from './profile-routing.module';
 import { ProfileComponent } from './containers/profile/profile.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UserAPI } from '../admin/containers/user/api/user.api';
+import { UserFacade } from '../admin/containers/user/user.facade';
+import { InstitutionAPI } from '../admin/containers/institution/api/institution.api';
+import { InstitutionFacade } from '../admin/containers/institution/institution.facade';
+import { StateAPI } from 'src/app/shared/api/state.api';
+import { StateFacade } from 'src/app/shared/services/state.facade';
 
 
 @NgModule({
@@ -11,10 +18,17 @@ import { ProfileComponent } from './containers/profile/profile.component';
   ],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild(ProfileRoutes)
   ],
   providers: [
-    
+    UserAPI,
+    UserFacade,
+    InstitutionAPI,
+    InstitutionFacade,
+    StateAPI,
+    StateFacade
   ]
 })
 export class ProfileModule { }
