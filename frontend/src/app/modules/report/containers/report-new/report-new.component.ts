2fac4b8bd7133f34dedd2c3a8926886399aabde8
Isac <isac.aniceto@hotmail.com>
2022-01-17 22:46:41 -0300

    Add the new report page

--- frontend/src/app/modules/report/containers/report-new/report-new.component.ts
+++ frontend/src/app/modules/report/containers/report-new/report-new.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { UserFacade } from 'src/app/modules/admin/containers/user/user.facade';
+import { ChallengeFacade } from 'src/app/modules/challenge/challenge.facade';
+import { SubscriptionsContainer } from 'src/app/shared/utils/subscriptions-container';
+import { ReportFacade } from '../../report.facade';
 
 @Component({
   selector: 'app-report-new',
   templateUrl: './report-new.component.html',
   styleUrls: ['./report-new.component.scss']
 })
-export class ReportNewComponent implements OnInit {
+export class ReportNewComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  challengeList = [] as any[];
+  userList = [] as any[];
+
+  reportForm: FormGroup;
+
+  subscriptions = new SubscriptionsContainer();
+  
+  constructor(
+    private formBuilder: FormBuilder,
+    private reportFacade: ReportFacade,
+    private challengeFacade: ChallengeFacade,
+    private userFacade: UserFacade
+  ) {
+    this.reportForm = this.formBuilder.group({
+      description: '',
+      idChallenge: null,
+      idAuthor: null
+    });
+
+    this.subscriptions.add = challengeFacade.getAll().subscribe(response => this.challengeList = response);
+    this.subscriptions.add = userFacade.getAll().subscribe(response => this.userList = response);
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    const payload = this.reportForm.value;
+    this.subscriptions.add = this.reportFacade.post(payload).subscribe(response => console.log(response));
+  }
+
+  changeChallenge(event: any) {
+    this.reportForm.get('idChallenge')?.setValue(+event.target.value);
+  }
+
+  changeAuthor(event: any) {
+    this.reportForm.get('idAuthor')?.setValue(+event.target.value);
+  }
+
+
+
+
+  ngOnDestroy(): void {
+      this.subscriptions.dispose();
   }
 
 }
--- frontend/src/app/modules/report/report.module.ts
+++ frontend/src/app/modules/report/report.module.ts
@@ -8,6 +8,10 @@ import { ReportNewComponent } from './containers/report-new/report-new.component
 import { ReportRoutes } from './report-routing.module';
 import { ReportAPI } from './api/classroom.api';
 import { ReportFacade } from './report.facade';
+import { ChallengeAPI } from '../challenge/api/challenge.api';
+import { ChallengeFacade } from '../challenge/challenge.facade';
+import { UserAPI } from '../admin/containers/user/api/user.api';
+import { UserFacade } from '../admin/containers/user/user.facade';
 
 
 
@@ -25,7 +29,11 @@ import { ReportFacade } from './report.facade';
   ],
   providers: [
     ReportAPI,
-    ReportFacade
+    ReportFacade,
+    ChallengeAPI,
+    ChallengeFacade,
+    UserAPI,
+    UserFacade
   ]
 })
 export class ReportModule { }
