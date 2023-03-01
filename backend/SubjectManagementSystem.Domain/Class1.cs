6be0b79831d186d16fcafa71e6ac57c77286f48b
Isac <isac.aniceto@hotmail.com>
2023-02-28 21:35:00 -0300

    Add docker-compose on the project

--- backend/SubjectManagementSystem.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace SubjectManagementSystem.Domain
-{
-    public class Class1
-    {
-    }
-}
--- frontend/src/app/core/services/base.api.ts
+++ frontend/src/app/core/services/base.api.ts
@@ -1,38 +1,39 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http"
-import { map, Observable } from "rxjs";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
-    providedIn: HttpClient
+  providedIn: HttpClient,
 })
-
 export abstract class BaseApi<T> {
-    constructor(protected http: HttpClient) { }
+  constructor(protected http: HttpClient) {}
 
-    public get baseUrl(): string {
-        return 'http://localhost:5000/api';
-    }
+  public get baseUrl(): string {
+    return 'http://172.17.0.1:5000/api';
+    // return 'http://localhost:5000/api';
+  }
 
-    protected get(url: string, params?: any): Observable<any> {
-        return this.http.get<T>(url, { params }).pipe(map((response: T) => response ));
-    }
+  protected get(url: string, params?: any): Observable<any> {
+    return this.http
+      .get<T>(url, { params })
+      .pipe(map((response: T) => response));
+  }
 
-    protected getOne(url: string, id: number): Observable<any> {
-        const urlFull = `${url}/${id}`;
-        return this.http.get<T>(urlFull).pipe(map((response: T) => response));
-    }
+  protected getOne(url: string, id: number): Observable<any> {
+    const urlFull = `${url}/${id}`;
+    return this.http.get<T>(urlFull).pipe(map((response: T) => response));
+  }
 
-    protected post(url: string, payload: T): Observable<any> {
-        return this.http.post<T>(url, payload);
-    }
+  protected post(url: string, payload: T): Observable<any> {
+    return this.http.post<T>(url, payload);
+  }
 
-    protected put(url: string, payload: T): Observable<any> {
-        return this.http.put<T>(url, payload);
-    }
+  protected put(url: string, payload: T): Observable<any> {
+    return this.http.put<T>(url, payload);
+  }
 
-    protected delete(url: string, id: number): Observable<any> {
-        const urlFull = `${url}/${id}`
-        return this.http.delete<T>(urlFull);
-    }
-
+  protected delete(url: string, id: number): Observable<any> {
+    const urlFull = `${url}/${id}`;
+    return this.http.delete<T>(urlFull);
+  }
 }
